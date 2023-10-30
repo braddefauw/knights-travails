ce4924272b2ec647df54c48ae045c465ccb3665c
Brad DeFauw <badefauw@gmail.com>
2023-10-30 10:49:16 -0500

    modify the code to track and output the path once the target position is reached

--- script.js
+++ script.js
@@ -1,108 +1,66 @@
-// define the size of the chessboard
-const boardSize = 8;
+function knightMoves(start, end){
+    // define the size of the chessboard
+    const boardSize = 8;
 
-// create an empty chessboard as a 2D array
-function createEmptyBoard(size){
-    const board = [];
-    for(let i = 0; i < size; i++){
-        board.push(Array(size).fill(0))
-    }
-    return board
-}
-
-// function to initialize the chessboard
-function initializeChessboard(size){
-    const board = createEmptyBoard(size);
-    return board;
-}
-
-// function to initialize the knight
-function initializeKnight(board, position){
-    const knight = {
-        board: board,
-        position: position,
-    };
-    return knight;
-}
-
-// function to check if a move is within the bounds of the chessboard 
-function isValidMove(x, y){
-    return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
-}
-
-// function to get all valid knight moves from a given position
-function getValidKnightMoves(position){
-    const moves = [
-        [-2, 1],
-        [-1, 2],
-        [1, 2],
-        [2, 1],
-        [2, -1],
-        [1, -2],
-        [-1, -2],
-        [-2, -1],
-    ]
-
-    const validMoves = [];
-
-    for (const move of moves){
-        const x = position[0] + move[0];
-        const y = position[1] + move[1];
-        if(isValidMove(x, y)){
-            validMoves.push([x, y]);
-        }
+    // function to check if a move is within the bounds of the chessboard 
+    function isValidMove(x, y){
+        return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
     }
 
-    return validMoves;
-}
+    // function to get all valid knight moves from a given position
+    function getValidKnightMoves(position){
+        const moves = [
+            [-2, 1],
+            [-1, 2],
+            [1, 2],
+            [2, 1],
+            [2, -1],
+            [1, -2],
+            [-1, -2],
+            [-2, -1],
+        ]
 
-// function to initialize a tree node
-function initializeTree(){
-    return {
-        position: null,
-        children: [],
-    };
-}
+        const validMoves = [];
 
-//function to build a tree of possible knight moves from initial to target position
-function buildKnightMoveTree(initialPosition, targetPosition) {
-    const tree = initializeTree();
-    tree.position = initialPosition;
-
-    const queue = [tree];
-
-    while(queue.length > 0) {
-        const node = queue.shift();
-        const currentPosition = node.position;
-
-        if (JSON.stringify(currentPosition) === JSON.stringify(targetPosition)){
-            // target position found, exit the loop
-            break;
+        for (const move of moves){
+            const x = position[0] + move[0];
+            const y = position[1] + move[1];
+            if(isValidMove(x, y)){
+                validMoves.push([x, y]);
+            }
         }
 
-        const validMoves = getValidKnightMoves(currentPosition);
+        return validMoves;
+    }
+
+    // initialize the BFS queue and visited set
+    const queue = [];
+    const visited = new Set();
+    queue.push({position: start, path: [start]});
+    visited.add(JSON.stringify(start));
+
+    while(queue.length > 0){
+        const {position, path } = queue.shift();
+        if(JSON.stringify(position) === JSON.stringify(end)){
+            // target position found
+            return `You made it in ${path.length - 1} moves! Here's your path:\n${path.map((square) => `[${square}]`).join("\n")}`;
+        }
+
+        const validMoves = getValidKnightMoves(position);
 
         for(const move of validMoves){
-            const child = {
-                position: move,
-                children: [],
-            };
-            node.children.push(child);
-            queue.push(child);
+            if(!visited.has(JSON.stringify(move))){
+                visited.add(JSON.stringify(move));
+                queue.push({position: move, path: [...path, move] });
+            }
         }
     }
 
-    return tree;
+    // if no path is found
+    return "No valid path found."
 }
 
 // example usage
-const chessboard = initializeChessboard(boardSize);
-const initialPosition = [0, 0]; // initial position of the knight
-const targetPosition = [3,3];
-
-const knight = initializeKnight(chessboard, initialPosition);
-const moveTree = buildKnightMoveTree(initialPosition, targetPosition);
-
-console.log("Chessboard:", chessboard)
-console.log("Knight's Initial Position:", knight)
-console.log("Move Tree:", moveTree)
+const start = [3,3]
+const end = [4,3]
+console.log(knightMoves(start, end))
