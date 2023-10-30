cf6732b9d066a2f78357243626221ac63789d96d
Brad DeFauw <badefauw@gmail.com>
2023-10-30 10:33:33 -0500

    Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board.

--- script.js
+++ script.js
@@ -25,10 +25,84 @@ function initializeKnight(board, position){
     return knight;
 }
 
+// function to check if a move is within the bounds of the chessboard 
+function isValidMove(x, y){
+    return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
+}
+
+// function to get all valid knight moves from a given position
+function getValidKnightMoves(position){
+    const moves = [
+        [-2, 1],
+        [-1, 2],
+        [1, 2],
+        [2, 1],
+        [2, -1],
+        [1, -2],
+        [-1, -2],
+        [-2, -1],
+    ]
+
+    const validMoves = [];
+
+    for (const move of moves){
+        const x = position[0] + move[0];
+        const y = position[1] + move[1];
+        if(isValidMove(x, y)){
+            validMoves.push([x, y]);
+        }
+    }
+
+    return validMoves;
+}
+
+// function to initialize a tree node
+function initializeTree(){
+    return {
+        position: null,
+        children: [],
+    };
+}
+
+//function to build a tree of possible knight moves from initial to target position
+function buildKnightMoveTree(initialPosition, targetPosition) {
+    const tree = initializeTree();
+    tree.position = initialPosition;
+
+    const queue = [tree];
+
+    while(queue.length > 0) {
+        const node = queue.shift();
+        const currentPosition = node.position;
+
+        if (JSON.stringify(currentPosition) === JSON.stringify(targetPosition)){
+            // target position found, exit the loop
+            break;
+        }
+
+        const validMoves = getValidKnightMoves(currentPosition);
+
+        for(const move of validMoves){
+            const child = {
+                position: move,
+                children: [],
+            };
+            node.children.push(child);
+            queue.push(child);
+        }
+    }
+
+    return tree;
+}
+
 // example usage
 const chessboard = initializeChessboard(boardSize);
 const initialPosition = [0, 0]; // initial position of the knight
+const targetPosition = [3,3];
+
 const knight = initializeKnight(chessboard, initialPosition);
+const moveTree = buildKnightMoveTree(initialPosition, targetPosition);
 
 console.log("Chessboard:", chessboard)
 console.log("Knight's Initial Position:", knight)
+console.log("Move Tree:", moveTree)
